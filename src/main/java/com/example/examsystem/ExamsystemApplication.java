ab35387ecadcaf48983ae4ee7165d05c40218f54
yywSpace <1095204049@qq.com>
2020-03-22 11:58:50 +0800

    创建SpringBoot工程

---
+++ src/main/java/com/example/examsystem/ExamsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.examsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examsystem/ExamsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
