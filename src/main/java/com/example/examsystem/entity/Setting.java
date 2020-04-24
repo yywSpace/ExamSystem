f64362327dece5f23e2108d9edfd5abf3549c775
yywSpace <1095204049@qq.com>
2020-04-24 12:40:43 +0800

    添加数据库依赖及admin增删改查

---
+++ src/main/java/com/example/examsystem/entity/Setting.java
@@ -0,0 +1,75 @@
+package com.example.examsystem.entity;
+
+public class Setting {
+    /**
+     * 后台扫描任务执行的周期 minute
+     */
+    private int dutyCycle;
+    /**
+     * 分页查询时的每页记录数、
+     */
+    private int pageCount;
+
+    /**
+     * 手动开启考试的时间阈值
+     */
+    private int timeThreshold;
+    /**
+     * 学生上传文件字节数的有效范围
+     */
+    private int uploadBytesUpper;
+    private int uploadBytesLower;
+
+    /**
+     *  是否允许主考教师清理和删除考试
+     */
+    private boolean allowClearAndDelete;
+
+    public int getDutyCycle() {
+        return dutyCycle;
+    }
+
+    public void setDutyCycle(int dutyCycle) {
+        this.dutyCycle = dutyCycle;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public int getTimeThreshold() {
+        return timeThreshold;
+    }
+
+    public void setTimeThreshold(int timeThreshold) {
+        this.timeThreshold = timeThreshold;
+    }
+
+    public int getUploadBytesUpper() {
+        return uploadBytesUpper;
+    }
+
+    public void setUploadBytesUpper(int uploadBytesUpper) {
+        this.uploadBytesUpper = uploadBytesUpper;
+    }
+
+    public int getUploadBytesLower() {
+        return uploadBytesLower;
+    }
+
+    public void setUploadBytesLower(int uploadBytesLower) {
+        this.uploadBytesLower = uploadBytesLower;
+    }
+
+    public boolean isAllowClearAndDelete() {
+        return allowClearAndDelete;
+    }
+
+    public void setAllowClearAndDelete(boolean allowClearAndDelete) {
+        this.allowClearAndDelete = allowClearAndDelete;
+    }
+}
---
+++ src/main/java/com/example/examsystem/mapper/AdminMapper.java
@@ -0,0 +1,20 @@
+package com.example.examsystem.mapper;
+
+import com.example.examsystem.entity.Admin;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface AdminMapper {
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into admin(name,password) values(#{name}, #{password})")
+    void insertAdmin(Admin admin);
+
+    @Update("update admin set name = #{name}, password = #{password} where id = #{id}")
+    void updateAdmin(Admin admin);
+
+    @Delete("delete from admin where id = #{id}")
+    void deleteAdminById(int id);
+
+    @Select("select * from admin where id = #{id}")
+    Admin getAdminById(int id);
+}
